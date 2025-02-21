6d981e98864d3b46b00c99e3d111a7dd923b8beb
Jun <j.yonemura.a@gmail.com>
2025-02-21 09:21:22 +0900

    記事詳細ページ (page.tsx) に React コンポーネントを追加 & 404ページ (not-found.tsx) を作成

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,9 +1,58 @@
-export default function Article({ params }: { params: { slug: string } }) {
-    return (
-      <div>
-        <h1>記事の詳細</h1>
-        <p>記事のスラッグ: {params.slug}</p>
-      </div>
-    );
+import { notFound } from "next/navigation";
+import { Article, Comment } from "../../types";
+
+const getArticle = async (slug: string) => {
+  const res = await fetch(`http://localhost:3000/api/articles/${slug}`, {
+    next: { revalidate: 60 },
+  });
+
+  if (res.status === 404) {
+    // notFound 関数を呼び出すと not-found.tsx を表示する
+    notFound();
   }
-  
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch article");
+  }
+
+  const data = await res.json();
+  return data as Article;
+};
+
+const getComments = async (slug: string) => {
+  const res = await fetch(
+    `http://localhost:3000/api/articles/${slug}/comments`,
+    {
+      cache: "no-store",
+    }
+  );
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch comments");
+  }
+
+  const data = await res.json();
+  return data as Comment[];
+};
+
+export default async function ArticleDetail({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const article = await getArticle(params.slug);
+  const comments = await getComments(params.slug);
+
+  return (
+    <div>
+      <h1>{article.title}</h1>
+      <p>{article.content}</p>
+      <h2>コメント一覧</h2>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}>{comment.body}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
